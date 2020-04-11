bee7809f1f450bc5d2a02169469e3789a6333a91
Irina Smirnova <kraizette@gmail.com>
2020-04-11 10:33:24 +0500

    UpdDisplay

--- Kursovoy/Applications/Output/Display/Driver/DisplayDriver.hpp
+++ Kursovoy/Applications/Output/Display/Driver/DisplayDriver.hpp
@@ -3,11 +3,11 @@
 
 #pragma once
 
-#include "thread.hpp" //for thread
-#include "SPI.hpp" //for thread
+#include "Pin.hpp" //for Pin
+#include "SPI.hpp" //for SPI
 #include "IDisplayDriver.hpp"  //for IDisplayDriver
 
-const unsigned char LUT_VCOM[] = {
+constexpr unsigned char LUT_VCOM[] = {
   0x00, 0x0E, 0x00, 0x00, 0x00, 0x01,        
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,        
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,        
@@ -17,7 +17,7 @@ const unsigned char LUT_VCOM[] = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
-const unsigned char LUT_W2W[] = {
+constexpr unsigned char LUT_W2W[] = {
   0xA0, 0x0E, 0x00, 0x00, 0x00, 0x01,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -27,7 +27,7 @@ const unsigned char LUT_W2W[] = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
-const unsigned char LUT_B2W[] = {
+constexpr unsigned char LUT_B2W[] = {
   0xA0, 0x0E, 0x00, 0x00, 0x00, 0x01,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -37,7 +37,7 @@ const unsigned char LUT_B2W[] = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00   
 };
 
-const unsigned char LUT_B2B[] = {
+constexpr unsigned char LUT_B2B[] = {
   0x50, 0x0E, 0x00, 0x00, 0x00, 0x01,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -47,7 +47,7 @@ const unsigned char LUT_B2B[] = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00   
 };
 
-const unsigned char LUT_W2B[] ={
+constexpr unsigned char LUT_W2B[] = {
 0x50, 0x0E, 0x00, 0x00, 0x00, 0x01,
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -83,10 +83,11 @@ public :
   DisplayDriver() {
     SPIConfig SPI2Config;
     SPI2Config.mode = Mode :: Master;
-    SPI2Config.dataformat = DataFormat :: Bit16;
+    SPI2Config.dataformat = DataFormat :: Bit8;
     SPI2Config.frameformat = FrameFormat :: MSB;
-    SPI2Config.baudrate = BaudRate :: DIV4;
+    SPI2Config.baudrate = BaudRate :: DIV2;
     SPI2Config.timingrelationships = TimingRelationships :: MODE11;
+ 
     SPI :: Config(SPI2Config);
     DIN :: SetAlternate();
     CLK :: SetAlternate();
@@ -98,26 +99,23 @@ public :
   
   void Init() override {
     Reset(); //EPD_4IN2BC_Reset();
-    RST :: Set(); //DEV_Digital_Write(EPD_RST_PIN, 1);
-    RST :: Reset(); //DEV_Digital_Write(EPD_RST_PIN, 0);
-    RST :: Set(); //DEV_Digital_Write(EPD_RST_PIN, 1);
-    
     SendCommand(Command::BoosterSoftStart);//EPD_4IN2BC_SendCommand(0x06); // BOOSTER_SOFT_START
-    SendData(0x17);//EPD_4IN2BC_SendData(0x17);
-    SendData(0x17);//EPD_4IN2BC_SendData(0x17);
-    SendData(0x17); //EPD_4IN2BC_SendData(0x17); // 07 0f 17 1f 27 2F 37 2f
+    SendData(0x17);//Booster Soft Start(BTST) BT_PHA[7:0] SPI
+    SendData(0x17);//Booster Soft Start(BTST) BT_PHB[7:0] SPI
+    SendData(0x17); //Booster Soft Start(BTST) BT_PHC[5:0] SPI
     SendCommand(Command::PowerOn);//EPD_4IN2BC_SendCommand(0x04); // POWER_ON
     while(BUSY.IsSet()) {}; //EPD_4IN2BC_ReadBusy();//0: busy, 1: idle
     SendCommand(Command::PanelSetting);//EPD_4IN2BC_SendCommand(0x00); // PANEL_SETTING
-    SendData(0x0F);//EPD_4IN2BC_SendData(0x0F); // LUT from OTP
+    SendData(0x0F); //Panel Setting (PSR) RES[1:0],REG,KW/R,UD,SHL,SHD_N,RST_N// LUT from OTP
     SetLut();
     SendCommand (Command :: VCMDCSetting);
-    SendData(0x12);  
+    SendData(0x12); //Display Refresh(DRF)
     SendCommand (Command :: VcomDataIntervalSetting);
-    SendCommand (0x97);
+    SendCommand (0x97); //VBDF 17|D7 VBDW 97  VBDB 57  VBDF F7  VBDW 77  VBDB 37  VBDR B7
   };
   
   void Clear() override {
+    const std::uint8_t BlackColor = 0xff;
     SetResolution();
     SendCommand(Command::DisplayStartTransmission1);
     for (int i = 0; i < W / 8 * H; i ++) {
@@ -133,13 +131,15 @@ public :
   void Display(uint8_t *buff, size_t lenght) override {
     SetResolution();
     if (buff != nullptr) {
+      for (std::uint16_t j = 0; j < H; j++) {
       SendCommand(Command :: DisplayStartTransmission2); 
-      for(int i = 0; i < W / 8 * H; i++) {
+      StartSendData();
+      for(std::uint16_t i = 0; i < W; i++) {
         SendData(buff[i+j*W]);
-      }                  
+      }
+      EndSendData();
     };
-    SendCommand (Command :: DisplayRefresh);
-    while(BUSY.IsSet()) {};
+    Refresh();
   };
   
 private :
@@ -150,16 +150,19 @@ private :
     EndSendData(); //DEV_Digital_Write(EPD_CS_PIN, 1);
   };
   
-  void SendData(uint8_t data) {
-    StartSendData();
+  void SendData(std::uint8_t data) {
+    //StartSendData();
     SPI :: WriteByte(data); //DEV_SPI_WriteByte(Data);
-    EndSendData();//DEV_Digital_Write(EPD_CS_PIN, 1);
+    //EndSendData();//DEV_Digital_Write(EPD_CS_PIN, 1);
   };
   
   void Reset() {
     RST :: Set(); //DEV_Digital_Write(EPD_RST_PIN, 1);
+    //for (int i = 0; i < 100000; i ++) {};
     RST :: Reset(); //DEV_Digital_Write(EPD_RST_PIN, 0);
+    //for (int i = 0; i < 100000; i ++) {};
     RST :: Set();//DEV_Digital_Write(EPD_RST_PIN, 1);
+    //for (int i = 0; i < 100000; i ++) {};
   };
   
   void Refresh() override {
@@ -169,10 +172,11 @@ private :
   
   void SetResolution() {
     SendCommand (Command :: ResolutionSetting);
+    const std::uint8_t BlackColor = 0xff;
     SendData(W >> 8);        
-    SendData(W & 0xff);
+    SendData(W & BlackColor);
     SendData(H >> 8);
-    SendData(H & 0xff); 
+    SendData(H & BlackColor); 
   };
   
   void StartSendData() {
